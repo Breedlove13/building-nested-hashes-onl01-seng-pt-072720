15b9091db6d52c7de7e563996e2bd928e31afa39
Brian Breedlove <b.breedlove@outlook.com>
2020-08-01 02:05:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => { 
-    :patriarch {},
+    :patriarch { },
     :matriarch {},
-    :hero {},
+    :hero { },
     :hero_friends [ ]
    
       }, 
     :capulet => {
-      :patriarch {},
-      :matriarch {},
-      :herione {},
+      :patriarch { },
+      :matriarch { },
+      :herione { },
       :herione_friends [ ]
       
       }}
